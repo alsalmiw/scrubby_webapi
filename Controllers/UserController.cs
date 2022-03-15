cfb91572b4354b9ef700a215fa53dd7df937e74f
Daniel Bautista <dbautista9015@gmail.com>
2022-03-15 15:36:21 -0700

    Removed Comments from UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -35,10 +35,5 @@ namespace scrubby_webapi.Models
         {
             return _data.UpdateUsername(id, username);
         }
-        // public bool UpdateUsername(int id, string username)
-        // {
-        // }
-
-
     }
 }

5880002930dae877d5efd9c4687a944b756817c9
pVang8 <pvang@codestack.co>
2022-03-16 14:05:54 -0700

    not much

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -11,7 +11,6 @@ namespace scrubby_webapi.Models
         public string? Username {get; set;}
         public string? Salt {get; set;}
         public string? Hash {get; set;}
-
          public int TrackerId { get; set; }
         //public int UserId { get; set; }
         public string? Photo { get; set; }

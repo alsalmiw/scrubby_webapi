1a9277c344fcd6a4030ed7170b3709dd0be943af
pVang8 <pvang@codestack.co>
2022-03-31 14:22:30 -0700

    move and remove wrong model

--- Services/Context/DataContext.cs
+++ Services/Context/DataContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using scrubby_webapi.Models;
+using scrubby_webapi.Models.Static;
 
 namespace scrubby_webapi.Services.Context
 {

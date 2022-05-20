a1d86b0b60dc1bc7fbf957537ad444a1b017ba89
alsalmiw <alsalmiw@gmail.com>
2022-05-20 10:02:05 -0700

    added photo

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -91,6 +91,7 @@ namespace scrubby_webapi.Services
                 UserModel newUser = new UserModel();
                 newUser.Id = UserToAdd.Id;
                 newUser.Username = UserToAdd.Username;
+                newUser.Photo = UserToAdd.Photo;
                 newUser.Name = UserToAdd.Fullname;
                 
                 var hashedPassword = HashPassword(UserToAdd.Password);

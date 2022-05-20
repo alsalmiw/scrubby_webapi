9835097cb29c776b05291f970b025fa66ec24fd2
pVang8 <pvang@codestack.co>
2022-05-19 22:36:39 -0700

    -addd fullname  to creating account -peter

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -91,7 +91,8 @@ namespace scrubby_webapi.Services
                 UserModel newUser = new UserModel();
                 newUser.Id = UserToAdd.Id;
                 newUser.Username = UserToAdd.Username;
-
+                newUser.Name = UserToAdd.Fullname;
+                
                 var hashedPassword = HashPassword(UserToAdd.Password);
 
                 newUser.Salt = hashedPassword.Salt;
